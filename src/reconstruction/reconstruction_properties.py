a624a460107f00987717eab1a4d2315358d1a774
skapur <jose.vieira153@gmail.com>
2019-01-28 12:02:51 +0000

    [ADD] iMAT properties class

--- src/reconstruction/reconstruction_properties.py
+++ src/reconstruction/reconstruction_properties.py
@@ -2,7 +2,7 @@ from cobamp.utilities.property_management import PropertyDictionary
 from cobamp.wrappers.external_wrappers import model_readers
 
 from reconstruction.methods_reconstruction import MethodsReconstruction
-
+from numpy import ndarray, array
 
 class PropertiesReconstruction(PropertyDictionary):
 	def __init__(self):
@@ -42,6 +42,30 @@ class GIMMEProperties(PropertiesReconstruction):
 		self['obj_frac'] = obj_frac if isinstance(obj_frac, ndarray) else array([obj_frac]*len(objectives))
 		self['preprocess'] = True if preprocess else False
 		self['flux_threshold'] = 1e-4 if flux_threshold is None else flux_threshold
+
+
+class IMATFamilyProperties(PropertiesReconstruction):
+	def __init__(self, exp_vector, exp_thresholds, core=None, tolerance=1e-8):
+		new_mandatory = {
+			'exp_vector': lambda x: isinstance(x, list) and len(x) > 0 or isinstance(x, ndarray),
+			'objectives': lambda x: type(x) in [list, ndarray],
+			'exp_threshold': lambda x: type(x) in (tuple, list, ndarray) and type(x[0]) in [float, int] and type(
+				x[1]) in [float, int]
+		}
+		new_optional = {
+			'core': lambda x: type(x) in [ndarray, list, tuple],
+			'tolerance': float
+		}
+		super().__init__()
+
+		self.add_new_properties(new_mandatory, new_optional)
+
+		self['exp_vector'] = exp_vector
+		self['exp_thresholds'] = exp_thresholds
+		if core:
+			self['core'] = core
+		if tolerance:
+			self['tolerance'] = tolerance
 
 if __name__ == '__main__':
 	properties = PropertiesReconstruction()
