f1a19bd5d568460cafe543c664723d0c76659a30
skapur <jose.vieira153@gmail.com>
2019-01-28 18:10:29 +0000

    [FINISHED] Working implementation and test for iMAT

--- src/reconstruction/methods/imat.py
+++ src/reconstruction/methods/imat.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import chain
 
 from cobamp.core.linear_systems import GenericLinearSystem, VAR_CONTINUOUS, VAR_BINARY
 from cobamp.core.optimization import LinearSystemOptimizer
@@ -20,39 +21,40 @@ class IMAT():
 		core = self.properties['core']
 		epsilon = self.properties['epsilon']
 
-		high_idx = np.where(exp_vector >= exp_ub)[0]
-		low_idx = np.where(0 <= exp_vector < exp_lb)[0]
+		high_idx = (np.where(exp_vector >= exp_ub)[0]).astype(int)
+		low_idx = (np.where((exp_vector >= 0) & (exp_vector < exp_lb))[0]).astype(int)
 
 		if core:
 			high_idx = np.union1d(high_idx, np.array(core))
 
-		lso, lsystem = generate_imat_problem(self, S, lb, ub, high_idx, low_idx, epsilon)
+		lso, lsystem = self.generate_imat_problem(self.S, self.lb, self.ub, high_idx, low_idx, epsilon)
 
 		solution = lso.optimize()
 
-		to_remove = abs(solution.x()) < tol
+		to_keep = np.where(abs(solution.x())[:self.S.shape[1]] >= tol)[0]
 
-		if solution.status() == 'optimal':
+		if solution.status() != 'optimal':
 			print('Solution was not optimal')
 
-		return to_remove
-
-
+		return to_keep
 
 	def generate_imat_problem(self, S, lb, ub, high_idx, low_idx, epsilon):
 		m,n = S.shape
 		nh, nl = len(high_idx), len(low_idx)
-		h_ident, l_ident = self.empty_matrix(nh, nl), self.empty_matrix(nh, nl)
 
-		h_ident[list(zip(range(nh),high_idx))] = 1
-		l_ident[list(zip(range(nl),low_idx))] = 1
+		h_ident, l_ident = self.empty_matrix(nh, n), self.empty_matrix(nl, n)
+		h_lb, h_ub, l_lb, l_ub = self.empty_matrix(nh,nh), self.empty_matrix(nh,nh), self.empty_matrix(nl,nl), self.empty_matrix(nl,nl)
+		h_diag, l_diag = np.diag_indices_from(h_lb), np.diag_indices_from(l_lb)
 
+		if nh > 0:
+			h_ident[(np.array(range(nh)), high_idx)] = 1
+			h_lb[h_diag] = lb[high_idx] - epsilon
+			h_ub[h_diag] = ub[high_idx] + epsilon
 
-		h_lb = np.fromfunction(lambda x,y: lb[high_idx[x]] - epsilon if x==y else 0, (nh, nh))
-		h_ub = np.fromfunction(lambda x,y: ub[high_idx[x]] + epsilon if x==y else 0, (nh, nh))
-
-		l_lb = np.fromfunction(lambda x,y: ub[low_idx[x]] if x==y else 0, (nl, nl))
-		l_ub = np.fromfunction((lambda x,y: ub[low_idx[x]] if x==y else 0, (nl, nl)))
+		if nl > 0:
+			l_ident[(np.array(range(nl)), low_idx)] = 1
+			l_lb[l_diag] = lb[low_idx]
+			l_ub[l_diag] = ub[low_idx]
 
 		rows = [
 			[S, self.empty_matrix(m,nh+nh+nl)],
@@ -62,23 +64,25 @@ class IMAT():
 			[l_ident, self.empty_matrix(nl,nh*2),l_ub]
 		]
 
-		filt = lambda lst,idx: list(filter(lambda x: x in idx, lst))
-
 		A = np.vstack(list(map(np.hstack,rows)))
-		b_lb = [0]*n + filt(lb, high_idx) + [None]*nh + filt(lb, low_idx) + [None]*nl
-		b_ub = [0]*n + [None]*nh + filt(ub, high_idx) + [None]*nl + filt(ub, low_idx)
+		b_lb = [0]*m + list(lb[high_idx]) + [None]*nh + list(lb[low_idx]) + [None]*nl
+		b_ub = [0]*m + [None]*nh + list(ub[high_idx]) + [None]*nl + list(ub[low_idx])
 
-		A_lb, A_ub = lb + [0]*(2*nh+nl), ub + [1]*(2*nh+nl)
+		A_lb, A_ub = np.concatenate([lb, np.array([0] * (2 * nh + nl))]), np.concatenate([ub, np.array([1] * (2 * nh + nl))])
 		A_vt = [VAR_CONTINUOUS]*n + [VAR_BINARY]*(2*nh+nl)
-		prefix_maker = lambda char, dim: [char+str(i) for i in range(dim)]
-		A_names = sum(list(map(prefix_maker,[('V',n),('Hpos',nh),('Hneg',nh),('L',nl)])))
+
+		## TODO: Move this to optimization on cobamp
+		prefix_maker = lambda cd: list([cd[0]+str(i) for i in range(cd[1])])
+		A_names = list(chain(*list(map(prefix_maker,[('V',n),('Hpos',nh),('Hneg',nh),('L',nl)]))))
 
 		lsystem = GenericLinearSystem(S=A, var_types=A_vt, lb=A_lb, ub=A_ub, b_lb=b_lb, b_ub=b_ub, var_names=A_names)
 		lso = LinearSystemOptimizer(lsystem)
 
-		A_f = np.zeros(len(A.shape[1]))
+		A_f = np.zeros((A.shape[1]))
 		A_f[n:] = 1
-		lsystem.set_objective(A_f, True)
-
+		lsystem.set_objective(A_f, False)
+		# DEBUG LINES
+		#print(high_idx, low_idx)
+		#lsystem.write_to_lp('imat.lp')
 		return lso, lsystem
 
---
+++ tests/imat_test.py
@@ -0,0 +1,27 @@
+import numpy as np
+from reconstruction.reconstruction_properties import IMATProperties
+from reconstruction.methods.imat import IMAT
+
+if __name__ == '__main__':
+
+	S = np.array([[1, -1, 0, 0, -1, 0, -1, 0, 0],
+				  [0, 1, -1, 0, 0, 0, 0, 0, 0],
+				  [0, 1, 0, 1, -1, 0, 0, 0, 0],
+				  [0, 0, 0, 0, 0, 1, -1, 0, 0],
+				  [0, 0, 0, 0, 0, 0, 1, -1, 0],
+				  [0, 0, 0, 0, 1, 0, 0, 1, -1]])
+	M, N = S.shape
+	lb = np.array([0] * N).astype(float)
+	lb[3] = -10
+	ub = np.array([10] * N).astype(float)
+
+	names = ['R' + str(i + 1) for i in range(N)]
+	exp_vector = np.random.random(N)
+
+	properties = IMATProperties(
+		exp_vector=exp_vector,
+		exp_thresholds=(0.2,0.5)
+	)
+
+	method = IMAT(S, lb, ub, properties)
+	print(['R'+str(i+1) for i in method.run()])
