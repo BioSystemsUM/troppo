221cc458ab58f2959b5096837906a3b299599c90
Jorge <jhmdf@hotmail.com>
2019-07-30 15:24:23 +0100

    [CHANGE] small changes

--- src/utilities/extra_information.py
+++ src/utilities/extra_information.py
@@ -1,20 +1,49 @@
 import pandas as pd
 import numpy as np
-from src.utilities.task import Task
+from utilities.task import Task
+import requests, zipfile, io
+
+class informationFromSeveralArticles():
+
+	def __init__(self):
+		pass
+
+	def hmr_to_recon_reaction_converter(self):
+		hmr_zip = requests.get("http://www.metabolicatlas.org/assets/hmr/HMRdatabase2_00.xlsx-07f0f2b5f71a03b0d6efdea752a9cebb.zip")
+		with zipfile.ZipFile(io.BytesIO(hmr_zip.content)) as my_zip:
+			hmr = pd.ExcelFile(my_zip.open('HMRdatabase2_00.xlsx'))
+
+		rx_hmr = hmr.parse(['RXNS'])['RXNS']
+		mapping = rx_hmr[['RXNID', 'BIGG DATABASE ID','COMPARTMENT']]
+		return mapping
+
+	def load_medium_rpmi1640(self, path_rpmi='./data/extra_information/rpmi1640.tsv'):
+		'''
+		This function loads the rpmi-1640 information from the paper https://www.nature.com/articles/srep45557
+		NOTE: this information is present in the Supplementary Infomartion, but the data is present on a .doc, so we
+		had to get that information, put it on a .tsv and then save it.
+		:return:
+		'''
+
+		rpmi = pd.read_csv(path_rpmi, delimiter = '\t').set_index('Reaction ID', drop = True)
+		map = self.hmr_to_recon_reaction_converter().set_index('RXNID', drop = True)
+		rpmi[map.columns] = map.loc[rpmi.index.values, :]
+
+		return rpmi
+
 
 class Nathan2019ConsensusPaper():
-
 	'''
 	This class has the purpose to import and make accessible the information present in the paper and in the
 	supplementary material
 	LINK : https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1006867
 	'''
 
-	def __init__(self):
-		self.root_path = './data/Nathan2019ConsensusPaper/'
+	def __init__(self, path):
+		self.root_path = path
 
 	def load_medium_constraints(self):
-		original = pd.read_excel(self.root_path + 'pcbi.1006867.s003.xlsx')
+		original = pd.read_excel(self.root_path + 'pcbi.1006867.s003.xlsx', index_col=0)
 		cell_lines = [col for col in original.columns if 'Unnamed' not in col]
 		gen_names = lambda x: (str(x) + '_LB', str(x) + '_UB')
 		new_cols = [bound for pair in [gen_names(cell) for cell in cell_lines] for bound in pair]
@@ -33,23 +62,18 @@ class Nathan2019ConsensusPaper():
 
 	def load_metabolic_tasks(self):
 		original = pd.read_excel(self.root_path + 'pcbi.1006867.s005.xlsx')
-		idx = np.where(original.ID.isna()==False)[0]
-		idx = np.append(idx, original.shape[0]-1)
-		return {i+1: Task(original.iloc[idx[i]:idx[i + 1], :]) for i in range(len(idx) - 1)}
+		idx = np.where(original.ID.isna() == False)[0]
+		idx = np.append(idx, original.shape[0] - 1)
+		return {i + 1: Task(original.iloc[idx[i]:idx[i + 1], :]) for i in range(len(idx) - 1)}
 
 	def load_hallmarks_genes(self):
 		original = pd.read_excel(self.root_path + 'pcbi.1006867.s013.xlsx', header=1)
 		return original
 
+
 if __name__ == '__main__':
 	path = './data/Nathan2019ConsensusPaper/'
 
-	n = Nathan2019ConsensusPaper()
+	n = Nathan2019ConsensusPaper(path)
 	x = n.load_medium_constraints()
 	c, v, b = n.load_biomass()
-	# z = pd.read_excel(path + 'pcbi.1006867.s005.xlsx')
-	# original = pd.read_excel(path + 'pcbi.1006867.s005.xlsx')
-	# idx = np.where(original.ID.isna() == False)[0]
-	# idx = np.append(idx, original.shape[0] - 1)
-	# tasks = {i+1: Task(original.iloc[idx[i]:idx[i + 1], :]) for i in range(len(idx) - 1)}
-	original = pd.read_excel(path + 'pcbi.1006867.s013.xlsx', header=1)
