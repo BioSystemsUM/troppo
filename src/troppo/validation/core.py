f09f86a86e9a61e75369d81a34581c8ad939a0e1
Vítor Vieira <skapur@users.noreply.github.com>
2020-10-22 14:58:56 +0100

    [ADD] Context-specific model simulator for benchmarking purposes

--- src/troppo/validation/core.py
+++ src/troppo/validation/core.py
@@ -1,4 +1,5 @@
 from cobamp.wrappers.core import ConstraintBasedModelSimulator
+from cobamp.utilities.hash import get_unique_dicts
 
 from numpy import nan
 
@@ -12,27 +13,34 @@ def default_post_processing_func(x):
 
 
 class ContextSpecificModelSimulator(object):
-    def __init__(self, model_simulator: ConstraintBasedModelSimulator, flux_data: dict, scenarios: dict,
+    def __init__(self, model_simulator: ConstraintBasedModelSimulator, scenarios=None,
                  post_process=None):
         self.model_simulator = model_simulator
-        self.data = flux_data
-        self.scenarios = scenarios
+        self.scenarios = scenarios if scenarios is not None else {'default':{}}
         self.post_process = default_post_processing_func if post_process is not None else post_process
 
-    def __simulate(self, contexts: dict, simulation_function, objective_coefficients: dict, minimize: bool, **kwargs):
+
+    def simulate(self, contexts: dict, simulation_function, objective_coefficients: dict, minimize: bool, **kwargs):
         context_names, context_bounds = zip(contexts.items())
 
         full_cnames, full_cbounds = [], []
         for sc_name, sc_bounds in self.scenarios:
+
             full_cnames.extend([(sc_name, c_name) for c_name, c_bounds in contexts.items()])
             context_bounds_scen = [{k:v for k,v in d.items()} for d in context_bounds]
             for d in context_bounds_scen: d.update(sc_bounds)
             full_cbounds.extend(context_bounds_scen)
 
+        dict_cache, dict_indices = get_unique_dicts(full_cbounds)
+        dnames, dobjs = zip(dict_cache.items())
+
         simulations = zip(*self.model_simulator.batch_simulate(func=simulation_function,
-                                            bound_changes=full_cbounds,
+                                            bound_changes=dobjs,
                                             objective_coefficients=[objective_coefficients],
                                             minimize=[minimize],
                                             **kwargs))
 
-        return dict(zip(full_cnames,[self.post_process(k) for k in simulations]))
+        pre_result = dict(zip(dnames,[self.post_process(k) for k in simulations]))
+
+        result_list = [pre_result[k] for k in dict_indices]
+        return dict(zip(full_cnames, result_list))
