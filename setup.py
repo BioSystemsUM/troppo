eed6759eb3384558bc3b40366bf3ebf64a4bb605
ao60337 <ao60337@gmail.com>
2023-01-25 10:42:51 +0000

    [REFACTOR] Refactoring of setup file.

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'troppo',
-    version = '0.0.5',
-    package_dir = {'':'src'},
-    packages = find_packages('src'),
-    install_requires = ["cobamp"],
+    name='troppo',
+    version='0.0.6',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=["cobamp==0.2.1"],
 
-    author = 'Jorge Ferreira & Vítor Vieira',
-    author_email = 'jorge.ferreira@ceb.uminho.pt & vvieira@ceb.uminho.pt',
-    description = 'TROPPO - Tissue-specific RecOnstruction and Phenotype Prediction using Omics data',
-    license = 'GNU General Public License v3.0',
-    keywords = 'pathway analysis metabolic model',
-    url = 'https://github.com/BioSystemsUM/troppo',
-    long_description = open('README.rst').read(),
-    classifiers = [
+    author='Jorge Ferreira & Vítor Vieira',
+    author_email='jorge.ferreira@ceb.uminho.pt',
+    description='TROPPO - Tissue-specific RecOnstruction and Phenotype Prediction using Omics data',
+    license='GNU General Public License v3.0',
+    keywords='pathway analysis metabolic model',
+    url='https://github.com/BioSystemsUM/troppo',
+    long_description=open('README.rst').read(),
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Intended Audience :: Science/Research',
