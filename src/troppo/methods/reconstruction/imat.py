9a0682d290c3d73d04fa6e7be9bb8a36998e3e06
ao60337 <ao60337@gmail.com>
2023-10-17 10:43:35 +0100

    [REFACTOR] Refactor of the iMAT algorithm.

--- src/troppo/methods/reconstruction/imat.py
+++ src/troppo/methods/reconstruction/imat.py
@@ -9,7 +9,26 @@ from troppo.methods.base import ContextSpecificModelReconstructionAlgorithm, Pro
 
 
 class IMATProperties(PropertiesReconstruction):
-    def __init__(self, exp_vector, exp_thresholds, core=None, tolerance=1e-8, epsilon=1):
+    """
+    Properties for IMAT algorithm
+
+    Parameters
+    ----------
+    exp_vector : list
+        The vector of expression values
+    objectives : list
+        The list of objectives
+    exp_thresholds : tuple
+        The thresholds for the expression values
+    core : list, optional
+        The core reactions, by default None
+    tolerance : float, optional
+        The tolerance, by default 1e-8
+    epsilon : float, optional
+        The epsilon, by default 1
+    """
+    def __init__(self, exp_vector: list, exp_thresholds: tuple or list or ndarray,
+                 core: ndarray or list or tuple = None, tolerance: float = 1e-8, epsilon: int or float = 1):
         new_mandatory = {
             'exp_vector': lambda x: isinstance(x, list) and len(x) > 0 or isinstance(x, ndarray),
             'objectives': lambda x: type(x) in [list, ndarray],
@@ -35,18 +54,53 @@ class IMATProperties(PropertiesReconstruction):
             self['epsilon'] = epsilon
 
     @staticmethod
-    def from_integrated_scores(scores, **kwargs):
+    def from_integrated_scores(scores: list, **kwargs) -> 'IMATProperties':
+        """
+        Create IMAT properties from integrated scores
+
+        Parameters
+        ----------
+        scores: list
+            The list of integrated scores
+        kwargs: dict
+            Additional arguments
+
+        Returns
+        -------
+        IMATProperties: The IMAT properties
+
+        """
         return IMATProperties(exp_vector=scores, **kwargs)
 
 
 class IMAT(ContextSpecificModelReconstructionAlgorithm):
+    """
+    IMAT algorithm
+
+    Parameters
+    ----------
+    S : ndarray
+        The stoichiometric matrix
+    lb : ndarray
+        The lower bounds
+    ub : ndarray
+        The upper bounds
+    properties : IMATProperties
+        The IMAT properties
+
+    Attributes
+    ----------
+    sol : LinearSystemOptimizer
+        The solution
+    """
+
     properties_class = IMATProperties
 
     @staticmethod
     def empty_matrix(r, c):
         return np.zeros((r, c))
 
-    def __init__(self, S, lb, ub, properties):
+    def __init__(self, S: ndarray, lb: ndarray, ub: ndarray, properties: IMATProperties):
         super().__init__(S, lb, ub, properties)
         self.S = np.array(S)
         self.lb, self.ub = np.array(lb), np.array(ub)
@@ -54,6 +108,14 @@ class IMAT(ContextSpecificModelReconstructionAlgorithm):
         self.sol = None
 
     def run_imat(self):
+        """
+        Run the IMAT algorithm
+
+        Returns
+        -------
+        solution: Solution
+            Instance with the solution to the IMAT problem
+        """
         exp_vector = self.properties['exp_vector']
         exp_lb, exp_ub = self.properties['exp_thresholds']
         core = self.properties['core']
@@ -71,6 +133,14 @@ class IMAT(ContextSpecificModelReconstructionAlgorithm):
         return solution
 
     def run(self):
+        """
+        Run the algorithm and return a list of reactions to keep in the final model.
+
+        Returns
+        -------
+        list: The list of reactions to keep in the final model
+
+        """
         tol = self.properties['tolerance']
         solution = self.run_imat()
         to_keep = np.where(abs(solution.x())[:self.S.shape[1]] >= tol)[0]
@@ -83,6 +153,31 @@ class IMAT(ContextSpecificModelReconstructionAlgorithm):
         return to_keep
 
     def generate_imat_problem(self, S, lb, ub, high_idx, low_idx, epsilon):
+        """
+        Generate the IMAT problem
+
+        Parameters
+        ----------
+        S: ndarray
+            The stoichiometric matrix
+        lb: ndarray
+            The lower bounds
+        ub: ndarray
+            The upper bounds
+        high_idx: ndarray
+            The high index
+        low_idx: ndarray
+            The low index
+        epsilon: float or int
+            The epsilon value
+
+        Returns
+        -------
+        lso: LinearSystemOptimizer
+            The linear system optimizer
+        lsystem: GenericLinearSystem
+            The linear system
+        """
         m, n = S.shape
         nh, nl = len(high_idx), len(low_idx)
 
