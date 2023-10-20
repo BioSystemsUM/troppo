b5e083c5a50692f6d9edcb534aa6c66aeae8b72d
ao60337 <ao60337@gmail.com>
2023-10-20 10:54:32 +0100

    [REFACTOR] Docstrings for the Utilities

--- src/troppo/utilities/statistics.py
+++ src/troppo/utilities/statistics.py
@@ -3,20 +3,46 @@
 import pandas as pd
 
 
-def normalize(data):
+def normalize(data: pd.DataFrame) -> pd.DataFrame:
+	"""
+	Normalize the given data
 
+	Parameters
+	----------
+	data: pd.DataFrame
+
+	Returns
+	-------
+	pd.DataFrame
+
+	"""
 	# if single:
 	# 	normalized_data = np.array([(n-min(data))/(max(data)-min(data)) if isnan(n) != True else -1 for n in data])
 	#
 	# else:
 	# 	normalized_data = None
 
-	return (data-data.min())/(data.max()-data.min())
+	return (data - data.min()) / (data.max() - data.min())
 
-def z_score(data, single=True):
+
+def z_score(data: pd.DataFrame, single=True) -> pd.DataFrame:
+	"""
+	Compute the Z-score of the given data
+
+	Parameters
+	----------
+	data: pd.DataFrame
+
+	single: bool, optional
+
+	Returns
+	-------
+	pd.DataFrame
+
+	"""
 	# TODO this is not Z-score, is mean/std
 	# if single:
 	# 	z_score = np.array([(n - mean(data)) / (std(data)) if isnan(n) != True else -1 for n in data])
 	# else:
 	# 	z_score = None
-	return (data-data.mean())/data.std()
+	return (data - data.mean()) / data.std()
