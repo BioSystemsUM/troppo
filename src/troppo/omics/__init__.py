2e8ec9cecd38b74c4ab6068e303a0d60e41540f0
ao60337 <ao60337@gmail.com>
2024-05-29 15:05:02 +0100

    [ADD] Added Gene Level Thresholding methods to the omics subpackage of Troppo.

--- src/troppo/omics/__init__.py
+++ src/troppo/omics/__init__.py
@@ -9,3 +9,4 @@ from .readers import (ProbeReader, HpaReader, TabularReader, GenericReader)
 from .core import (OmicsContainer, OmicsDataMap, TabularContainer, IdentifierMapping, OmicsMeasurementSet,
                   TypedOmicsMeasurementSet)
 from . import id_converter, integration
+from .gene_level_thresholding import GeneLevelThresholding
